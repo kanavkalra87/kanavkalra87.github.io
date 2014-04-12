9d0c12f101989be9dfefdf6ee3e6c4b50b01b4f7
kanavkalra87 <kalra.kanav@gmail.com>
2014-04-12 09:42:11 +0530

    Adding initial VS memorial sit
    Initial Commit

---
+++ js/CustomEvents.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('body').on('click', '.headerLink', function () {
+			$(document).attr("title", "Vivek Singh Memorial Trust - " + $(this).text());
+			$("#headerUL").find(".selected").removeClass("selected");
+			$(this).parent().addClass("selected");
+			$("#dynamicBody").load("htmlPages/" + $(this).attr('id') + ".html");
+	});
+	
+	$('body').on('click', '.highLightsLink', function () {
+			$("#overlayPopup").load("highlights/" + $(this).attr('id') + ".html");
+			$("#overlayPopup").popup("show");
+	});
+});
+

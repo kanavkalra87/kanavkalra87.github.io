d8249d4afcc06a459ca04a765b67a29a54f811e0
kanavkalra87 <kalra.kanav@gmail.com>
2014-06-07 14:26:06 +0530

    Added Projects tab
    Made changes for adding projects tab

--- js/CustomEvents.js
+++ js/CustomEvents.js
@@ -1,15 +1,39 @@
 $(document).ready(function(){
 	$('body').on('click', '.headerLink', function () {
-			$(document).attr("title", "Vivek Singh Memorial Trust - " + $(this).text());
-			$("#headerUL").find(".selected").removeClass("selected");
-			$(this).parent().addClass("selected");
+		$(document).attr("title", "Vivek Singh Memorial Trust - " + $(this).text());
+		$("#headerUL").find(".selected").removeClass("selected");
+		$(this).parent().addClass("selected");
+		if($(this).text() == "Projects"){
+			$("#generalBody").hide();
+			$("#projectsBody").show();
+			$("#projectsBody").load("htmlPages/" + $(this).attr('id') + ".html");
+		}else{
+			$("#generalBody").show();
+			$("#projectsBody").hide();
 			$("#dynamicBody").load("htmlPages/" + $(this).attr('id') + ".html");
+		}
 	});
 	
 	$('body').on('click', '.highLightsLink', function () {
-			$("#overlayPopup").html("Loading...");
-			$("#overlayPopup").load("highlights/" + $(this).attr('id') + ".html");
-			$("#overlayPopup").popup("show");
+		$("#overlayPopup").html("Loading...");
+		$("#overlayPopup").load("highlights/" + $(this).attr('id') + ".html");
+		$("#overlayPopup").popup("show");
+	});
+	
+	$('body').on('click', '.projectsLink', function () {
+		$("#projectsBody").load("projects/" + $(this).attr('id') + ".html");
+		//$("#projectsBody").load("projects/gallery-details.html");
+	});
+	$('body').on('click', '.projectMoreDetails', function () {
+		//$("#overlayPopup").html("Loading...");
+		//$("#overlayPopup").load("projects/" + $(this).attr('id') + ".html");
+		//$("#overlayPopup").popup("show");
+		$("#projectsBody").load("projects/" + $(this).attr('id') + ".html")
+	});
+	$('body').on('click', '.returnToProjects', function () {
+		$("#generalBody").hide();
+		$("#projectsBody").show();
+		$("#projectsBody").load("htmlPages/projects.html");
 	});
 });
 
